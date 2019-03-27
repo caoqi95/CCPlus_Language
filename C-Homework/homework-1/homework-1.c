f572a8bd3d8e6ce74cfedcefee246c80f8cba9b2
caoqi95 <caoqi95@gmail.com>
2019-03-27 22:37:49 +0900

    Update

--- C-Homework/homework-1/homework-1.c
+++ C-Homework/homework-1/homework-1.c
@@ -1,73 +1,41 @@
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
-
-void get_input(char **str, int num);
-void print_out(char **str, int num);
-void free_pointer(char **str, int num);
-int charstrcmp(char *s, char *t);
-int initstrcmp(char *s, char *t);
-void swap(char **s, char **t);
-void bubble_sort(void *str[],int num, int (*compare)(char *, char *));
-
-
-/*
-	function get_input(): get the input string array
-*/ 
-void get_input(char **str, int num)
-{
-	char *tmp;
-	char tmp_str[100];
-	int i, j;
-	for(i=0; i<num; i++)
-	{
-		printf("\nPlease input the Number.%d string: \n", i+1);
-		scanf("%s", &tmp_str);
-		str[i] = (char *)malloc((strlen(tmp_str)+1)*sizeof(char));
-		strcpy(str[i], tmp_str);
-	}
-}
-
 
 /*
 	function ptint_out(): print out the strings 
 */
-void print_out(char **str, int num)
-{	
+
+void print_out(char *str[], int num)
+{
+	printf("{ ");
 	int i;
 	for(i=0; i<num; i++)
 	{
-		printf("%s  ", str[i]);
+		printf("%s ", str[i]);
 	}
- } 
- 
-
-/*
-	function free_point(): free used string pointers
-*/
-void free_pointer(char **str, int num)
-{
-	int i;
-	for(i=0;i<num;i++)
-	{
-		free(str[i]);		
-	}
-
+	printf("}");
 }
 
 /*
     function charstrcmp(): compare two strings based on ASCII
 */
-int charstrcmp(char *s, char *t)
+int charstrcmp(char *s, char *t)//
 {
-    int i = 0;
-    while (s[i] != '\0' || t[i] != '\0')
-    {
-        if (s[i] > t[i]) { return 1; }
-        else if (s[i] < t[i]) { return -1; }
-        i++;
-    }
-    return 0;
+	int i=0;
+	
+	while(s[i]!='\0' || t[i]!='\0')
+	{
+		if(s[i] > t[i]) 
+		{
+			return 1;
+		}
+		else if(s[i] < t[i])
+		{
+			return -1;
+		}
+		i++;
+	}
+	return 0;
 }
 
 /*
@@ -98,62 +66,57 @@ int initstrcmp(char *s, char *t)
 	return 0;
 }
 
+
 /*
-	funciton swap(): swap two strings
+	funciton swap(): swap two pointers
 */
 void swap(char **s, char **t)
 {
-    char *tmp;
-    tmp = *s;
-    *s = *t;
-    *t = tmp;
-    tmp = NULL;
+	char *tmp;
+	tmp = *s;
+	*s = *t;
+	*t = tmp;
+	tmp = NULL;
 }
 
 /*
     function bubble_sort(): bubble sort for strings
 */
-void bubble_sort(void *str[], int num, int(*compare)(char *, char *))
+void bubble_sort(void *str[], int num, int(*compare)(char *, char*))
 {
-    int i, j;
-	for(i=0;i<=num-1;i++)
+	int i, j;
+	for(i=0; i<num-1; i++)
 	{
-		for(j=0;j<num-1;j++)
-		{
-			// if str[j-1] > sstr[j], should swap them
-            if (compare((char *)str[j],(char *)str[j+1])>0)
-            {
-                swap(&str[j], &str[j+1]);
-            }
+		for(j=0; j<num-1; j++)
+		{	
+		    // if str[j-1] > sstr[j], should swap them
+			if(compare((char *)str[j], (char *)str[j+1]) > 0)
+			{
+				// swap the value of [j] and [j+1] 
+				swap(&str[j], &str[j+1]);
+			}
 		}
 	}
-		
+ } 
 
-}
-
-
-
-
+/*
+	test program for the charstrcmp() and initstrcmp()
+*/
 
 int main()
 {
-	char **str;
-	int num, method;
-	// Input the number of strings and strings 
-	printf("\n=================== Input Number ====================\n");
-	printf("\nNumber of string:\n");
-	scanf("%i",&num);
-	printf("\n=================== Input Strings ===================\n");
-	str=(char **)malloc(num*sizeof(char *));
-	get_input(str, num);
-	
-	// Print out original strings 
+	// test strings 
+	int num=6;
+	char *str[] = {"3", "27", "123", "5", "9", "1"};
+
+
 	printf("\nOriginal strings: \n");
 	print_out(str, num);
-
-	// Chose sort method
-	printf("\n=================== Chose Method ====================\n");
-	printf("\nThere are two methods you can chose to sort:\n");
+	
+	// Choose sort method
+	int method;
+	printf("\n=================== Choose Method ====================\n");
+	printf("\nThere are two methods you can choose to sort:\n");
 	printf("\n1.ASCII;\n2.Integer Value\n");
 	printf("\nPlease input the method number you wanted:\n");
 	scanf("%d",&method);
@@ -165,10 +128,17 @@ int main()
 	printf("\nResult of %s method:\n", (method==1)?"ASCII":"Integer Value");
 	print_out(str,num);
 	
-	// Free pointers 
-	free_pointer(str,num);
-	free(str);
-	
-    return 1;
+
+	return 1;
 }
+
+
+
+
+
+
+
+
+
+
 
