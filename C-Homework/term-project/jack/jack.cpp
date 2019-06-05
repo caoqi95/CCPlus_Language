c65f653fcb70fb1e4b29f5b22ffda13305dacf14
caoqi95 <caoqi95@gmail.com>
2019-06-05 23:12:37 +0900

    Add Term Project

---
+++ C-Homework/term-project/jack/jack.cpp
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <algorithm>
+#include <conio.h>
+
+using namespace std; /* for cleaner code (min and max functions) */
+
+// Constants
+const int ncar_states = 21; // including "0" cars
+const int max_moves = 5;
+const int max_morning = ncar_states + max_moves;
+const double discount = 0.9;
+const double theta = pow(10, -7); // stop when differences are of order theta 
+
+// Probabilities
+double prob_1[max_morning][ncar_states]; // 26x21
+double prob_2[max_morning][ncar_states];
+
+// Rewards
+double rew_1[max_morning];
+double rew_2[max_morning];
+
+// Value and Policy
+double V[ncar_states][ncar_states];
+int policy[ncar_states][ncar_states];
+
+// Declarations
+double factorial(int n);
+double poisson(int n, double l);
+void load_probs_rewards(double probs[max_morning][ncar_states], double rewards[max_morning], 
+double l_reqsts, double l_drpffs);
+void policy_eval();
+double backup_action(int n1, int n2, int a);
+int greedy_policy(int n1, int n2);
+bool update_policy_t();
+ 
+/* function for Factorial: n! */
+double factorial(int n){
+	
+	if (n>0) return (n*factorial(n - 1));
+	else return (1.0);
+}
+
+/* function for Poisson Disturibution */
+double poisson(int n, double lambda){
+	
+	return (exp(-lambda)*pow(lambda, (double) n)/factorial(n));
+} 
+
+/* function for Initialize */
+void load_probs_rewards(double probs[max_morning][ncar_states], double rewards[max_morning], 
+double l_reqsts, double l_drpffs){
+	double req_prob;
+	double drp_prob;
+	int satisfied_req;
+	int new_n;
+	for(int req=0; (req_prob = poisson(req, l_reqsts)) > theta; req++){
+		// fill the reward matrix rewards[max_morning] using the probability req_prob
+		for(int n=0; n < max_morning; n++){
+			satisfied_req = min(req, n); // at most, all the cars avaliable
+			rewards[n] += 10 * req_prob * satisfied_req; // + 10 is the reward per request
+		}
+		for (int drp=0; (drp_prob = poisson(drp, l_drpffs)) > theta; drp++){
+			// fill the probability matrix probs[max_morning][ncar_states] 
+			for (int m=0; m < max_morning; m++){
+				satisfied_req = min(req, m);
+				new_n = m + drp - satisfied_req; 
+				new_n = max(new_n, 0); // 0 at least
+				new_n = min(20, new_n); // 20 at most
+				probs[m][new_n] += req_prob * drp_prob; // add up the joint probability
+			}
+		}
+	}
+}
+
+
+double backup_action(int n1, int n2, int a){
+	double val; // determine the range of possible actions for the given state
+	a = min(a, +n1);
+	a = max(a, -n2);
+	a = min(+5, a);
+	a = max(-5, a);
+	val = -2 * fabs((double) a);
+	
+	int morning_n1 = n1 - a;
+	int morning_n2 = n2 + a;
+	for (int new_n1 = 0; new_n1 < ncar_states; new_n1 ++){
+		for (int new_n2 = 0; new_n2 < ncar_states; new_n2++){
+			val += prob_1[morning_n1][new_n1]*prob_2[morning_n2][new_n2]*(rew_1[morning_n1] + 
+			rew_1[morning_n2] + discount * V[new_n1][new_n2]);
+		}
+	}
+	return val;
+}
+
+/* function for Policy Eualuation */
+void policy_eval(){
+	double val_tmp;
+	double diff;
+	int a;
+	
+	do{
+		diff = 0.0;
+		for(int n1=0; n1 < ncar_states; n1++){
+			for (int n2=0; n2 < ncar_states; n2++){
+				// Assign the new value for each state; keep in diff the highest update difference
+				val_tmp = V[n1][n2];
+				a = policy[n1][n2];
+				V[n1][n2] = backup_action(n1, n2, a);
+				diff = max(diff, fabs(V[n1][n2] - val_tmp));
+			}
+		}
+	} while (diff > theta);
+}
+
+
+bool update_policy_t(){
+	int b;
+	bool has_changed = false;
+	for (int n1=0; n1 < ncar_states; n1++){
+		for (int n2=0; n2 < ncar_states; n2++){
+			b = policy[n1][n2];
+			policy[n1][n2] = greedy_policy(n1, n2);
+			if (b != policy[n1][n2]){
+				has_changed = true;
+			}
+		}
+	}
+	return (has_changed);
+}
+
+int greedy_policy(int n1, int n2){
+	// set the range of available actions
+	int a_min = max(-5, -n2);
+	int a_max = min(+5, +n1);
+	double val;
+	double best_val;
+	double best_action;
+	int a;
+	
+	a = a_min;
+	best_action = a_min;
+	best_val = backup_action(n1, n2, a);
+	for (a=a_min+1; a <= a_max; a++){
+		val = backup_action(n1, n2, a);
+		if (val > best_val + pow(10, -9)){
+			best_val = val;
+			best_action = a;
+		}
+	} 
+	return (best_action);
+}
+
+void print_policy(){
+	printf("\nPolicy:\n");
+	for (int n1=0; n1 < ncar_states; n1 ++){
+		printf("\n");
+		for (int n2=0; n2 < ncar_states; n2++){
+			printf("%2d ", policy[ncar_states - (n1+1)][n2]);
+		}
+	}
+	printf("\n\n");
+}
+
+int main()
+{
+	double lambda_1r = 3.0; // request rate at location1 
+	double lambda_1d = 3.0; // dropoff rate at location1
+	double lambda_2r = 4.0; // request rate at location2
+	double lambda_2d = 2.0; // Dropoff rate at location2
+	
+	// Initialization
+	load_probs_rewards(prob_1, rew_1, lambda_1r, lambda_1d); /* 1st locatoin */
+	load_probs_rewards(prob_2, rew_2, lambda_2r, lambda_2d); /* 2nd location */
+	
+	print_policy();
+	// Policy Iteration
+	bool has_changed;
+	do{
+		// iterative policy evaluation 
+		policy_eval();
+		// improve the policy; assign true to has_changed if the policy changed, if it did not 
+		has_changed = update_policy_t();
+		// print policy result
+		print_policy();
+	} while (has_changed);
+
+	//getch();
+	
+	return 0;
+}
---
+++ C-Homework/term-project/jack/plot_policy.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun  5 22:46:47 2019
+
+@author: caoqi95
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def txt2arr():
+    
+    # read txt file
+    f = open('policy.txt', 'r')
+    lines = f.readlines()
+    
+    # delete spaces 
+    nums = []
+    for i in lines:
+        if len(i) > 10:
+            nums.append(i)
+            
+    # string to int
+    real_nums = []
+    for i in range(len(nums)):
+        nums[i] = nums[i].strip()
+        sp = nums[i].split(' ')
+        one_line_nums = []
+        for n in range(len(sp)):
+            if sp[n] != '':
+                one_line_nums.append(int(sp[n]))
+        real_nums.append(one_line_nums)
+    
+    # convert to array
+    policy_lists = [ real_nums[n*21:(n+1)*21] for n in range(int(len(real_nums)/21))]
+    policy_lists = np.array(policy_lists)
+    
+    return policy_lists
+
+def plot_policy(policy):
+    
+    A = np.arange(0, 21)
+    B = np.arange(0, 21)
+    A, B = np.meshgrid(A, B)
+    
+    levels = range(-5, 6, 1)
+    plt.figure(figsize=(18, 10))
+    for i in range(policy.shape[0]):
+        plt.subplot(231+i)
+        Po = policy[i][::-1]
+        CS = plt.contourf(A, B, Po, levels)
+        cbar = plt.colorbar(CS)
+        cbar.ax.set_ylabel('actions')
+        plt.title('Policy {}'.format(i))
+        plt.xlabel("Cars at Location 2")
+        plt.ylabel("Cars at Location 1")
+    plt.savefig('C:/Users/User/Desktop/result.jpg')
+    plt.show()
+    
+if __name__ == "__main__":
+    
+    policy = txt2arr()
+    plot_policy(policy)
+    
