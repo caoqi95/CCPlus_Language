f7ce9e849a37408e63f7346e3d704335959d871c
caoqi95 <caoqi95@gmail.com>
2019-06-09 13:39:16 +0900

    Modified jack.cpp

--- C-Homework/term-project/jack.cpp
+++ C-Homework/term-project/jack.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <string.h>
 #include <algorithm>
-
+#define RENTAL_INCOME 10 // 10 is the reward per request
 
 using namespace std; /* for cleaner code (min and max functions) */
 
@@ -22,7 +22,7 @@ double prob_2[max_morning][ncar_states];
 double rew_1[max_morning];
 double rew_2[max_morning];
 
-// Value and Policy
+// Value function and Policy
 double V[ncar_states][ncar_states];
 int policy[ncar_states][ncar_states];
 
@@ -54,17 +54,17 @@ void load_probs_rewards(double probs[max_morning][ncar_states], double rewards[m
 double l_reqsts, double l_drpffs){
 	double req_prob;
 	double drp_prob;
-	int satisfied_req;
+	int satisfied_req; // the number of satisfied request
 	int new_n;
-	for(int req=0; (req_prob = poisson(req, l_reqsts)) > theta; req++){
+	for(int req = 0; (req_prob = poisson(req, l_reqsts)) > theta; req++){
 		// fill the reward matrix rewards[max_morning] using the probability req_prob
-		for(int n=0; n < max_morning; n++){
+		for(int n = 0; n < max_morning; n++){
 			satisfied_req = min(req, n); // at most, all the cars avaliable
-			rewards[n] += 10 * req_prob * satisfied_req; // 10 is the reward per request
+			rewards[n] += RENTAL_INCOME * req_prob * satisfied_req; 
 		}
-		for (int drp=0; (drp_prob = poisson(drp, l_drpffs)) > theta; drp++){
+		for (int drp = 0; (drp_prob = poisson(drp, l_drpffs)) > theta; drp++){
 			// fill the probability matrix probs[max_morning][ncar_states] 
-			for (int m=0; m < max_morning; m++){
+			for (int  = 0; m < max_morning; m++){
 				satisfied_req = min(req, m);
 				new_n = m + drp - satisfied_req; 
 				new_n = max(new_n, 0); // 0 at least
@@ -122,7 +122,6 @@ bool policy_improvement(){
 	for (int n1=0; n1 < ncar_states; n1++){
 		for (int n2=0; n2 < ncar_states; n2++){
 			b = policy[n1][n2];
-			// get a new policy
 			policy[n1][n2] = greedy_policy(n1, n2);
 			if (b != policy[n1][n2]){
 				has_changed = true;
@@ -146,7 +145,7 @@ int greedy_policy(int n1, int n2){
 	best_val = estimate_action(n1, n2, a);
 	for (a=a_min+1; a <= a_max; a++){
 		val = estimate_action(n1, n2, a);
-		if (val > best_val + theta){
+		if (val > best_val + pow(10, -9)){
 			best_val = val;
 			best_action = a;
 		}
@@ -166,30 +165,6 @@ void print_policy(){
 	printf("\n\n");
 }
 
-/*
-void print_prob_1(){
-	printf("\n");
-	for (int n1=0; n1 < max_morning; n1++){
-		printf("\n");
-		for (int n2=0; n2 <ncar_states; n2++){
-			printf("%.4f ", prob_1[n1][n2]);
-		}
-	}
-	printf("\n");
-} 
-
-void print_prob_2(){
-	printf("\n");
-	for (int n1=0; n1 < max_morning; n1++){
-		printf("\n");
-		for (int n2=0; n2 <ncar_states; n2++){
-			printf("%.4f ", prob_2[n1][n2]);
-		}
-	}
-	printf("\n");
-} 
-*/
-
 int main()
 {
 	double lambda_1r = 3.0; // request rate at location1 
@@ -198,11 +173,8 @@ int main()
 	double lambda_2d = 2.0; // Dropoff rate at location2
 	
 	// Initialization
-	load_probs_rewards(prob_1, rew_1, lambda_1r, lambda_1d); /* 1st location */
+	load_probs_rewards(prob_1, rew_1, lambda_1r, lambda_1d); /* 1st locatoin */
 	load_probs_rewards(prob_2, rew_2, lambda_2r, lambda_2d); /* 2nd location */
-	
-	//print_prob_1();
-	//print_prob_2();
 	
 	print_policy();
 	// Policy Iteration
@@ -212,9 +184,11 @@ int main()
 		policy_eval();
 		// improve the policy; assign true to has_changed if the policy changed, if it did not 
 		has_changed = policy_improvement();
-		// print policy results
+		// print policy result
 		print_policy();
 	} while (has_changed);
 
+	//getch();
+	
 	return 0;
 }
